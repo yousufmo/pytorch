2598743d51012655143fa93d2cf3f6282b339e01
angelayi <yiangela7@gmail.com>
2024-04-04 16:30:54 -0700

    [effects] Add inductor support for tokens
    ghstack-source-id: f8f6ba6d4e8c48fbad2b399939e60baf6c00431f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/122347

--- torch/_inductor/graph.py
+++ torch/_inductor/graph.py
@@ -323,6 +323,8 @@ class GraphLowering(torch.fx.Interpreter):
         )
         self.init_backend_registration()
 
+        self.effectful_ops: Dict[Any, ir.Buffer] = {}
+
     @staticmethod
     def decide_layout_opt(gm, *, is_inference) -> bool:
         """
@@ -625,7 +627,10 @@ class GraphLowering(torch.fx.Interpreter):
         self.buffers.append(buffer)
         self.name_to_buffer[name] = buffer
         # Skip empty CPU tensor so that CUDA graphs can succeed, see https://github.com/pytorch/pytorch/pull/114144
-        if not isinstance(buffer, ir.ComputedBuffer) or not buffer.is_zero_elements():
+        if (
+            not (isinstance(buffer, ir.ComputedBuffer) and buffer.is_zero_elements())
+            and buffer.get_device() is not None
+        ):
             self.add_device_info(buffer.get_device())
         return name
 
@@ -884,6 +889,7 @@ class GraphLowering(torch.fx.Interpreter):
                     sympy.Expr,
                     sympy.logic.boolalg.Boolean,
                     int,
+                    ir.EffectfulKernel,
                 ),
             )
             for x in result
